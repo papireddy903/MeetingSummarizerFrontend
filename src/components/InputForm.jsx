71277c90711546a21879d662dd2163533f2ceead
Papireddy <papireddyeppala903@gmail.com>
2025-08-17 14:48:59 +0530

    first commit

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -21,7 +21,7 @@ const InputForm = () => {
     formData.append("prompt", prompt);
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/api/summarize", {
+      const res = await fetch("https://meetingsummarizerbackend-4mh9.onrender.com/api/summarize", {
         method: "POST",
         body: formData,
       });
